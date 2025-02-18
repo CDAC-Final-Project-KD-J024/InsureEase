700a849c25ade90154384dbdc363920dd440fc1c
Mayank Choudhary <mayankchoudhary6055@gmail.com>
2025-02-18 19:12:21 +0530

    App.jsx and Main.jsx resolved

--- insure-ease-frontend/src/main.jsx
+++ insure-ease-frontend/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// main.jsx - Entry Point
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Provider} from 'react-redux';
+import store from './store/store'
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "react-toastify/dist/ReactToastify.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
