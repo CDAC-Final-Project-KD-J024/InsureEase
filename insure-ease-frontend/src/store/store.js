f51ba41162dbf777baf3898f11dc076c5495c3d3
Mayank Choudhary <mayankchoudhary6055@gmail.com>
2025-02-18 15:25:56 +0530

    store status modified

---
+++ insure-ease-frontend/src/store/store.js
@@ -0,0 +1,22 @@
+// store/store.js
+// store modified
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../slices/authSlice';
+import policyReducer from '../slices/policySlice';
+import userPolicyReducer from '../slices/userPolicySlice';
+import claimReducer from '../slices/claimSlice';
+import adminReducer from '../slices/adminSlice';
+import orderReducer from '../slices/orderSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    policies: policyReducer,
+    userPolicies: userPolicyReducer,
+    claims: claimReducer,
+    admin: adminReducer,
+    orders: orderReducer,
+  },
+});
+
+export default store;
